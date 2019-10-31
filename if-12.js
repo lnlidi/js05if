653c312d9c73a955d0bdb17e86efe72def1ec7a6
Ayhal <54825050+Ayhal@users.noreply.github.com>
2019-10-31 23:21:19 +0800

    Add files via upload

--- if-12.js
+++ if-12.js
@@ -1,10 +1,43 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
-
-function ...(){
-
-
-}
-
-module.exports = ...;
+function solve(){alert(digit_name(4))}
+var digit_name = (function () {
+  var names = ['0', '1', '2', '3','4','5','6','7','8','9'];
+  
+  return function () {
+   switch(names[n])
+      {
+      	 case '0' :
+            System.out.println("zero"); 
+            break;
+         case '1' :
+            System.out.println("one"); 
+            break;
+         case '2' :
+            System.out.println("two"); 
+            break;
+         case '3' :
+            System.out.println("three");
+            break;
+         case '4' :
+            System.out.println("four");
+            break;
+         case '5' :
+            System.out.println("five");
+            break;
+         case '6' :
+            System.out.println("six"); 
+            break;
+         case '7' :
+            System.out.println("seven"); 
+            break;
+         case '8' :
+            System.out.println("eight"); 
+            break;
+         case '9' :
+            System.out.println("nine"); 
+            break;
+         default :
+            System.out.println("Error");
+}}();
