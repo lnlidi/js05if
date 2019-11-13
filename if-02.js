60e54df4d39523c9fb25bf4450840c283959a0aa
Ayhal <vasilev2001as@gmail.com>
2019-11-14 00:55:24 +0900

    Сделал

--- if-02.js
+++ if-02.js
@@ -1,14 +1,12 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
-
-function f(a,b,c,d){
-var min1,min2,max;
+ function f(a,b,c,d){
+ var min1,min2,max;
  if (a<=b) 
-   	{min1=a}else{min1=b}
+     {min1=a}else{min1=b}
  if (c<=d)
-    {min2=c}else{min2=d}
+     {min2=c}else{min2=d}
  if (min1>=min2)
-    {return max=min1}else{return max=min2}
-}
-module.exports =  f;
+     {return max=min1}else{return max=min2}}
+ module.exports =  f;
--- if-12.js
+++ if-12.js
@@ -1,42 +1,39 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
-function f(n) {
-   switch(n)
-      {
-      	case 0 :
-            f = ("zero"); 
-            break;
+ function f(n){
+     switch(n){
+         case 0 :
+             f = "zero";
+             break;
          case 1 :
-            f = ("one"); 
-            break;
+             f = "one";
+             break;
          case 2 :
-            f = ("two"); 
-            break;
+             f = "two";
+             break;
          case 3 :
-            f = ("three");
-            break;
+             f = "three";
+             break;
          case 4 :
-            f = ("four");
-            break;
+             f = "four";
+             break;
          case 5 :
-            f = ("five");
-            break;
+             f = "five";
+             break;
          case 6 :
-            f = ("six"); 
-            break;
+             f = "six";
+             break;
          case 7 :
-            f = ("seven"); 
-            break;
+             f = "seven"; 
+             break;
          case 8 :
-            f = ("eight"); 
-            break;
+             f = "eight"; 
+             break;
          case 9 :
-            f = ("nine"); 
-            break;
+             f = "nine"; 
+             break;
          default :
-            f = ("Error");
-}
-return f;
-}
-module.exports =  f;
+             f = "error";} 
+     return f;}
+ module.exports =  f;
